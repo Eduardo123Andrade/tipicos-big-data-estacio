5c96ef8a536c8e5b27238adec04222d5b3b0e7e9
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2024-10-07 20:30:45 -0300

    created insert executor

---
+++ repository/exec_insert_query.py
@@ -0,0 +1,14 @@
+from mysql_connection import get_connection as _get_connection
+
+def exec_query(query =""):
+    connection = _get_connection()
+    cursor = connection.cursor()
+    cursor.execute(query)
+
+    connection.commit()
+    # result = cursor.fetchall()
+    
+    cursor.close()
+    connection.close()
+
+    # return result
