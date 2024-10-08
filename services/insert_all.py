8ae6179340c223783e65cedd1f37c5eb26cc4b42
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2024-10-07 21:58:04 -0300

    created repository to insert cvli crimes

--- services/insert_all.py
+++ services/insert_all.py
@@ -1,14 +1,14 @@
 
-import repository.insert_cities as ri
-
-ri.insert_cities()
+# import repository.insert_cities as ri
+import repository.insert_city_crimes as rcc
 
 def insert_all_data():
-  try:
-    ri.insert_cities()
+  # try:
+    # ri.insert_cities()
+  rcc.insert_city_crimes()
     # print()
-  except:
-    print("data already exists")
+  # except:
+  #   print("data already exists")
   
   print("finish")
   
