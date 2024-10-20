4c8985200e126676a7dc9801b2367e1adc240b1e
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2024-10-19 23:14:20 -0300

    create script to insert mdfcm crimes

---
+++ repository/insert_city_crimes_mdfcm.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os
+import repository.exec_insert_query as eq
+
+
+def insert_city_crimes_mdfcm():
+  path = f'{os.path.abspath(".")}/data_base/xlsx/VDFCM.xlsx'
+  df_vdfcm = pd.read_excel(path)
+  column_0 = df_vdfcm.columns[0]
+  
+  year_columns = df_vdfcm.columns[1:]
+
+  relation_data = ""
+  
+  cities = df_vdfcm[column_0].values[2: -1].tolist()
+
+  for city in cities:
+    index = cities.index(city)
+    print(city)
+    for column in year_columns:
+      year = int(df_vdfcm[column].values[1])
+      quantity = int(df_vdfcm[column].values[index + 3])
+      relation_data += f"""(
+        {year}, 
+        {quantity}, 
+        (SELECT m.id FROM Municipio m WHERE m.nome = '{city}'), 
+        (SELECT c.id FROM Crime c WHERE c.sigla = 'VDFCM')),
+        """
+
+  insert_crime_municipio = f"""
+  INSERT INTO TOPICOS_BIGDATA.Crime_Municipio
+  (ano, quantidade, municipio_id, crime_id)
+  VALUES
+  """
+  insert_crime_municipio += f"{relation_data[:-10]};"
+  
+  eq.exec_query(insert_crime_municipio)
--- services/insert_all.py
+++ services/insert_all.py
@@ -3,6 +3,7 @@
 import repository.insert_city_crimes_cvli as rcc_cvli
 import repository.insert_city_crimes_ccp as rcc_ccp
 import repository.insert_city_crimes_mvi as rcc_mvi
+import repository.insert_city_crimes_mdfcm as rcc_mdfcm
 
 def insert_all_data():
   # try:
@@ -10,6 +11,7 @@ def insert_all_data():
   # rcc_cvli.insert_city_crimes_cvli()
   # rcc_ccp.insert_city_crimes_ccp()
   # rcc_mvi.insert_city_crimes_mvi()
+  # rcc_mdfcm.insert_city_crimes_mdfcm()
     # print()
   # except:
   #   print("data already exists")
