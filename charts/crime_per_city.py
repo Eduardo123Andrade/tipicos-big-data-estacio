ddcb754899c6830136f13f60c8f5d105f2b88bce
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2024-10-20 08:23:35 -0300

    updated chart style

--- charts/crime_per_city.py
+++ charts/crime_per_city.py
@@ -3,8 +3,8 @@ import pandas as pd
 import plotly.express as px
 
 
-def crimes_per_city(city_ids, initial_year=None, final_year=None):
-  result = cpc.get_crime_per_city(city_ids=city_ids, initial_year=initial_year, final_year=final_year)
+def crimes_per_city(city_ids, crime_id, initial_year=None, final_year=None):
+  result = cpc.get_crime_per_city(city_ids=city_ids, crime_id=crime_id, initial_year=initial_year, final_year=final_year)
 
   return _format_data_frame(result)
 
@@ -14,16 +14,20 @@ def _format_data_frame(query_result):
     'Cidade',
     'Descrição',
     'Sigla', 
-    'Quantidade'
+    'Ano',
+    'Quantidade',
   ])
   
-  fig_date = px.bar(
+  fig_date = px.line(
     df, 
-    x="Sigla",
+    x="Ano",
     y="Quantidade",
-    title="Crimes na Cidade",
+    
+    color="Cidade",
+    title="Evolução de Crimes por Cidade",
     labels="Descrição",
     hover_data="Descrição"
   )
+
 
   return fig_date.update_traces(text=df["Quantidade"])
--- repository/crime_per_city.py
+++ repository/crime_per_city.py
@@ -2,12 +2,13 @@ from repository.set_date_filter_on_query import set_date_on_query
 from utils.add_cities_list_on_query import add_cities_id_list_on_query
 import repository.exec_query as eq
 
-def _query_build(city_ids, initial_year, final_year=None):
+def _query_build(city_ids, crime_id, initial_year, final_year=None):
     query = '''
     SELECT
       m.nome,
       c.descricao,
       c.sigla,
+      cm.ano,
       COALESCE(SUM(cm.quantidade), 0) AS qtd_crime
     FROM Crime_Municipio cm
     LEFT JOIN Municipio m ON cm.municipio_id = m.id
@@ -15,6 +16,7 @@ def _query_build(city_ids, initial_year, final_year=None):
     WHERE
 	    1 = 1
     '''
+    query += f"AND cm.crime_id = '{crime_id}'"
     query += add_cities_id_list_on_query(city_ids=city_ids)
 
     query += set_date_on_query(initial_year=initial_year, final_year=final_year)
@@ -23,12 +25,13 @@ def _query_build(city_ids, initial_year, final_year=None):
     GROUP BY
       m.nome,
       c.sigla,
-      c.descricao;
+      c.descricao,
+      cm.ano;
     '''
     return query
 
-def get_crime_per_city(city_ids, initial_year=None, final_year=None):
-    query = _query_build(city_ids=city_ids, initial_year=initial_year, final_year=final_year)
+def get_crime_per_city(city_ids, crime_id, initial_year=None, final_year=None):
+    query = _query_build(city_ids=city_ids, crime_id=crime_id, initial_year=initial_year, final_year=final_year)
     result = eq.exec_query(query=query)
     
     return result
