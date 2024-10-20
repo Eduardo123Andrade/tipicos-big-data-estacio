dafba7b5d6f037eaa36cbd0825c1943aff86ae85
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2024-10-19 22:50:45 -0300

    create script to insert mvi crimes

--- services/insert_all.py
+++ services/insert_all.py
@@ -2,12 +2,14 @@
 # import repository.insert_cities as ri
 import repository.insert_city_crimes_cvli as rcc_cvli
 import repository.insert_city_crimes_ccp as rcc_ccp
+import repository.insert_city_crimes_mvi as rcc_mvi
 
 def insert_all_data():
   # try:
     # ri.insert_cities()
   # rcc_cvli.insert_city_crimes_cvli()
   # rcc_ccp.insert_city_crimes_ccp()
+  # rcc_mvi.insert_city_crimes_mvi()
     # print()
   # except:
   #   print("data already exists")
